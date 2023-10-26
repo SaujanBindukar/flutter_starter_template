2a040de7cd376477eac8320baded857dc22c54a9
Saujan Bindukar <sauzanbindukar@gmail.com>
2023-10-26 23:13:59 +0545

    removed unused code

--- lib/core/widgets/custom_textfield.dart
+++ lib/core/widgets/custom_textfield.dart
@@ -1,103 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-// class CustomTextField extends StatefulWidget {
-//   CustomTextField({
-//     super.key,
-//     required this.controller,
-//     this.obscureText = false,
-//     this.hintText,
-//     this.focusNode,
-//     this.nextNode,
-//     this.onFieldSubmitted,
-//     this.textInputAction,
-//     this.textInputType,
-//     this.length,
-//     this.onTap,
-//     this.textCapitalization,
-//     this.labelText,
-//     this.inputFormatters,
-//     this.autofillHints,
-//     this.prefixIcon,
-//     this.validator,
-//   });
-//   final TextEditingController controller;
-//   bool obscureText;
-//   final String? hintText;
-//   final FocusNode? focusNode;
-//   final FocusNode? nextNode;
-//   final TextInputAction? textInputAction;
-//   final TextInputType? textInputType;
-//   final Function()? onTap;
-//   final TextCapitalization? textCapitalization;
-//   final List<TextInputFormatter>? inputFormatters;
-//   final void Function(String value)? onFieldSubmitted;
-//   final int? length;
-//   final String? labelText;
-//   final List<String>? autofillHints;
-//   final Widget? prefixIcon;
-//   final String? Function(String?)? validator;
-//   @override
-//   State<CustomTextField> createState() => _CustomTextFieldState();
-// }
-
-// class _CustomTextFieldState extends State<CustomTextField> {
-//   @override
-//   Widget build(BuildContext context) {
-//     void onFieldSubmitted(String text) {
-//       widget.onFieldSubmitted?.call(text);
-//       if (widget.nextNode != null) {
-//         widget.nextNode!.requestFocus();
-//       } else {
-//         FocusScope.of(context).unfocus();
-//       }
-//     }
-
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(vertical: 8.0),
-//       child: TextFormField(
-//         autofillHints: widget.autofillHints,
-//         enableSuggestions: false,
-//         textCapitalization:
-//             widget.textCapitalization ?? TextCapitalization.none,
-//         onTap: widget.onTap,
-//         controller: widget.controller,
-//         onFieldSubmitted: onFieldSubmitted,
-//         validator: widget.validator,
-//         inputFormatters: widget.inputFormatters ?? [],
-//         keyboardType: widget.textInputType,
-//         textInputAction: widget.textInputAction ??
-//             (widget.nextNode == null
-//                 ? TextInputAction.done
-//                 : TextInputAction.next),
-//         obscureText: widget.obscureText,
-//         decoration: InputDecoration(
-//           hintText: widget.hintText,
-//           labelText: widget.labelText,
-//           prefixIcon: widget.prefixIcon,
-//           suffixIcon: widget.hintText == 'Password'
-//               ? IconButton(
-//                   splashColor: Colors.transparent,
-//                   icon: widget.obscureText
-//                       ? const Icon(
-//                           Icons.visibility_off,
-//                         )
-//                       : const Icon(
-//                           Icons.visibility,
-//                         ),
-//                   onPressed: () {
-//                     setState(() {
-//                       widget.obscureText = !widget.obscureText;
-//                     });
-//                   },
-//                 )
-//               : const SizedBox(),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class CustomTextField extends StatefulWidget {
   const CustomTextField({
     super.key,
@@ -139,7 +42,7 @@ class CustomTextField extends StatefulWidget {
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
-  bool obscureText = false;
+  bool obscureText = true;
 
   @override
   Widget build(BuildContext context) {
